e8c1c59ba7f095b250f679af93d4074f9f2ba795
abdikadir-mohamed-dev <abdikadirmohammed180@gmail.com>
2026-04-17 10:22:17 +0300

    functions-lab

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-
+function calculateTax(amount) {
+  let tax = amount * 0.10;
+  return tax;
+}
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+function findMaximum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+function isPalindrome(word) {
+  let reversed = word.split('').reverse().join('');
+  
+  if (word === reversed) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  let discount = (discountPercentage / 100) * originalPrice;
+  let finalPrice = originalPrice - discount;
+  return finalPrice;
+}
 
 
 
